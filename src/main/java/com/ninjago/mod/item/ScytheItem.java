2b3dee5d7367f5982cbe7362223c1a716094ef73
shadowninja478903-hue <shadowninja478903@gmail.com>
2026-05-22 16:30:52 +0530

    Delete src/main directory

--- src/main/java/com/ninjago/mod/item/ScytheItem.java
+++
@@ -1,9 +0,0 @@
-package com.ninjago.mod.item;
-
-import net.minecraft.ChatFormatting;
-
-public class ScytheItem extends ElementalWeaponItem {
-    public ScytheItem() {
-        super("Earth", ChatFormatting.DARK_GREEN, 7, -2.4f);
-    }
-}
